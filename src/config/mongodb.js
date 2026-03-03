3c85c77c00781c06dc0f83a53cf7fbcad1b47bb0
Thanh_Nguyen <nguyenducthanh10121995@gmail.com>
2026-03-04 00:09:08 +0700

    test mcp server

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,35 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+// acc mongodb: thanh_nguyen (Thanh@10121995)
+
+const MONGODB_URI =
+  'mongodb+srv://thanh_nguyen:gwhZSRP1NWmwB3NN@cluster0.bjmyyhs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const DATABASE_NAME = 'trello'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+let trelloDatabaseInstance = null
+
+// Khởi tạo 1 doi tuong MongoClient de ket noi mongodb
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+// ket noi db
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect()
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+}
+
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) {
+    throw new Error('Must connect to database first')
+  }
+  return trelloDatabaseInstance
+}
