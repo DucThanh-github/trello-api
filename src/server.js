4db445089381a6d8d3a4f4a18a06391a3551e949
Thanh_Nguyen <nguyenducthanh10121995@gmail.com>
2026-03-04 00:09:08 +0700

    test mcp server

--- src/server.js
+++ src/server.js
@@ -1,26 +1,37 @@
+/* eslint-disable no-console */
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import { CONNECT_DB, GET_DB } from './config/mongodb.js'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  const hostname = 'localhost'
+  const port = 8017
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.get('/', (req, res) => {
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+  app.listen(port, hostname, () => {
+    console.log(`Hello , I am running at ${hostname}:${port}/`)
+  })
+}
+;(async () => {
+  try {
+    console.log('1> Starting server...')
+    await CONNECT_DB()
+    console.log('2> Connected to database')
+    START_SERVER()
+  } catch (error) {
+    console.error('Error starting server:', error)
+    process.exit(0)
+  }
+})()
+
+// CONNECT_DB()
+//   .then(() => console.log('connected to database'))
+//   .then(() => START_SERVER())
+//   .catch((err) => {
+//     console.error('Failed to connect to database:', err)
+//     process.exit(0)
+//   })
