347cb10c5bcac4d461c666636071810dce7c0113
Thanh_Nguyen <nguyenducthanh10121995@gmail.com>
2026-03-15 00:10:25 +0700

    feat: connect db and config env

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 /* eslint-disable no-console */
-import express from 'express'
-import { CONNECT_DB, GET_DB } from './config/mongodb.js'
 
+import express from 'express'
+import { CONNECT_DB, CLOSE_DB } from './config/mongodb.js'
+import env from './config/environment.js'
+import asyncExitHook from 'async-exit-hook'
 const START_SERVER = () => {
   const app = express()
-
-  const hostname = 'localhost'
-  const port = 8017
+  const hostname = env.APP_HOST
+  const port = env.APP_PORT
 
   app.get('/', (req, res) => {
     res.end('<h1>Hello World!</h1><hr>')
@@ -15,18 +16,23 @@ const START_SERVER = () => {
   app.listen(port, hostname, () => {
     console.log(`Hello , I am running at ${hostname}:${port}/`)
   })
+
+  asyncExitHook(() => {
+    console.log('Closing database connection...')
+    CLOSE_DB()
+  })
 }
-;(async () => {
-  try {
-    console.log('1> Starting server...')
-    await CONNECT_DB()
-    console.log('2> Connected to database')
-    START_SERVER()
-  } catch (error) {
-    console.error('Error starting server:', error)
-    process.exit(0)
-  }
-})()
+  ; (async () => {
+    try {
+      console.log('1> Starting server...')
+      await CONNECT_DB()
+      console.log('2> Connected to database')
+      START_SERVER()
+    } catch (error) {
+      console.error('Error starting server:', error)
+      process.exit(0)
+    }
+  })()
 
 // CONNECT_DB()
 //   .then(() => console.log('connected to database'))
