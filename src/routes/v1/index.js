0117c7085972dd850ad386b3e63a2567493754b7
Thanh_Nguyen <nguyenducthanh10121995@gmail.com>
2026-03-19 00:12:49 +0700

    setup router

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,15 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import express from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { BoardRoutes } from './boardRoutes.js';
+
+const Router = express.Router();
+
+Router.get('/status', (req, res) => {
+    return res.status(StatusCodes.OK).json({
+        message: 'OK'
+    })
+});
+
+Router.use('/boards', BoardRoutes)
+
+export const APIs_V1 = Router;
